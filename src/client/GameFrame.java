6b40abf96203ffee2d5d75f9e91e1f298cba12a9
xiaoy990 <xiaoy990@nenu.edu.cn>
2019-07-10 20:18:51 +0800

    添加键盘监听，按下一个键改变方块方向

--- src/client/GameFrame.java
+++ src/client/GameFrame.java
@@ -1,18 +1,41 @@
 package client;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class GameFrame extends Frame {
 
     int x = 100, y = 100;
+    int speed = 10;
 
     public GameFrame(){
 
         setLocation(100,100);
         setSize(800,600);
         setVisible(true);
+
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                switch (e.getKeyCode()){
+                    case KeyEvent.VK_LEFT:
+                        x -= speed;
+                        break;
+                    case KeyEvent.VK_UP:
+                        y -= speed;
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        x += speed;
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        y += speed;
+                        break;
+                }
+            }
+        });
 
         addWindowListener(new WindowAdapter() {
             @Override
@@ -25,6 +48,5 @@ public class GameFrame extends Frame {
     @Override
     public void paint(Graphics g){
         g.fillRect(x,y,60,60);
-        x++;
     }
 }
